7671c4a7f9f27a55c0003408ce07077cf3a3a191
ry-nishimura <72368098+ry-nishimura@users.noreply.github.com>
2020-10-31 23:23:59 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :nickname, presence: true
+  with_options presene: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '漢字、全角ひらがな、カタカナを使用してください'} do
+    validates :family_name
+    validates :first_name
+  end
+  with_options presene: true, format: { with: /\A[ァ-ン]+\z/, message: '全角カタカナを使用してください'} do
+    validates :family_name_kana
+    validates :first_name_kana
+  end
+  validates :birthday, presence: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
