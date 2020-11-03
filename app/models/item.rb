f67b6ba3b37f987734b74d10c7b5960c8650e2e9
ry-nishimura <72368098+ry-nishimura@users.noreply.github.com>
2020-11-03 19:35:33 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,11 @@ class Item < ApplicationRecord
   belongs_to :shipping_fee_type
   belongs_to :prefecture
   belongs_to :delivery_time
+
+  validates :name, :explanation, :category, :condition, :shipping_fee_type, :prefecture, :delivery_time, presence: true
+  validates :category_id, numericality: { other_than: 1 }
+  validates :condition_id, numericality: { other_than: 1 }
+  validates :shipping_fee_type, numericality: { other_than: 1 }
+  validates :prefecture, numericality: { other_than: 1 }
+  validates :delivery_time, numericality: {other_than: 1 }
 end
