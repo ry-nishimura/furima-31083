8ca8fab8315ab5cdc54043b4b8657be934858f8a
ry-nishimura <72368098+ry-nishimura@users.noreply.github.com>
2020-11-03 11:02:58 +0900

    FactryBotの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker: :Pokemon.name}
-    email                 {Faker: :Internet.free_email}
-    password              {Faker: :Internet.password(min_length: 6)}
+    nickname              {Faker::Pokemon.name}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
-    family_name           {Faker: :Name.family_name}
-    first_name            {Faker: :Name.first_name}
-    family_name_kana      {Faker: :Name.family.katakana}
-    first_name_kana       {Faker: :Name.first.katakana}
-    birthday              {Faker: :Faker::Date.backward}
+    family_name           {Faker::Name.family_name}
+    first_name            {Faker::Name.first_name}
+    family_name_kana      {Faker::Name.family.katakana}
+    first_name_kana       {Faker::Name.first.katakana}
+    birthday              {Faker::Faker::Date.backward}
   end
 end
