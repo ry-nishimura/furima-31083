833fad44b5c784a71ebc80739543c262024ea1e8
ry-nishimura <72368098+ry-nishimura@users.noreply.github.com>
2020-11-09 17:38:49 +0900

    データが正しく保存される場合、トップページへ。保存されなかった場合そのまま留まるよう設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,5 +7,16 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :explanation, :category_id, :condition_id, :shipping_fee_type_id, :prefecture_id, :delivery_time_id, :price, :image)
   end
 end
