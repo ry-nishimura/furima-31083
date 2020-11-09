52d7cff55202a83e50fac1b4f09f8a00d67bd53b
ry-nishimura <72368098+ry-nishimura@users.noreply.github.com>
2020-11-09 20:17:45 +0900

    itemsコントローラーのcreateアクションを修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,7 +7,8 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.create(item_params)
+    @item = Item.new(item_params)
+    # binding.pry
     if @item.save
       redirect_to root_path
     else
