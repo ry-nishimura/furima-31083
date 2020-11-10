38816e80559e1166e754fc25400c1cc46843de43
ry-nishimura <72368098+ry-nishimura@users.noreply.github.com>
2020-11-10 14:18:01 +0900

    FactoryBotを修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -8,6 +8,7 @@ FactoryBot.define do
     prefecture_id {Faker::Number.between(from: 2, to: 48)}
     delivery_time_id {Faker::Number.between(from: 2, to: 4)}
     price {Faker::Number.between(from: 300, to: 9999999)}
+    association :user
 
     after(:build) do |test|
       test.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
