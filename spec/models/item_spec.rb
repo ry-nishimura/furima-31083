e9e9265b6e4329861e960742b7297965b756aaf3
ry-nishimura <72368098+ry-nishimura@users.noreply.github.com>
2020-11-10 15:04:13 +0900

    単体テストコードを記述

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -8,26 +8,51 @@ RSpec.describe Item, type: :model do
   describe '商品の出品' do
     context "商品が出品できる場合" do
       it "入力必須項目が全て埋まっていれば、出品できる" do
+        expect(@item).to be_valid
       end
     end
     context "商品が出品できない場合" do
       it "画像がないと出品できない" do
       end
       it "商品名がないと保存できない" do
+        @item.name = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
       end
       it "商品説明がないと保存できない" do
+        @item.explanation = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Explanation can't be blank")
       end
       it "商品のカテゴリーが---だと保存できない" do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
       end
       it "商品の状態が---だと保存できない" do
+        @item.condition_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition must be other than 1")
       end
       it "配送料の負担が---だと保存できない" do
+        @item.shipping_fee_type_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping fee type must be other than 1")
       end
       it "発送元の地域が---だと保存できない" do
+        @item.prefecture_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
       end
       it "発送までの日数が---だと保存できない" do
+        @item.delivery_time_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery time must be other than 1")
       end
       it "価格がないと保存できない" do
+        @item.price = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
       end
       it "価格があっても、300円未満だと保存できない" do
       end
