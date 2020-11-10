1f8eaa098ec1ac0786219706afe586a7d7047cdd
ry-nishimura <72368098+ry-nishimura@users.noreply.github.com>
2020-11-10 14:09:25 +0900

    exampleの洗い出し

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品の出品' do
+    context "商品が出品できる場合" do
+      it "入力必須項目が全て埋まっていれば、出品できる" do
+      end
+    end
+    context "商品が出品できない場合" do
+      it "画像がないと出品できない" do
+      end
+      it "商品名がないと保存できない" do
+      end
+      it "商品説明がないと保存できない" do
+      end
+      it "商品のカテゴリーが---だと保存できない" do
+      end
+      it "商品の状態が---だと保存できない" do
+      end
+      it "配送料の負担が---だと保存できない" do
+      end
+      it "発送元の地域が---だと保存できない" do
+      end
+      it "発送までの日数が---だと保存できない" do
+      end
+      it "価格がないと保存できない" do
+      end
+      it "価格があっても、300円未満だと保存できない" do
+      end
+      it "価格があっても、10M円以上だと保存できない" do
+      end
+    end
+  end
 end
