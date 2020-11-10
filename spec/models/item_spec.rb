9321af90a445f0e6a734986416f6b3e180c9592b
ry-nishimura <72368098+ry-nishimura@users.noreply.github.com>
2020-11-10 15:57:11 +0900

    exampleの追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -12,6 +12,11 @@ RSpec.describe Item, type: :model do
       end
     end
     context "商品が出品できない場合" do
+      it "ユーザーが紐付いていないと商品を出品できない" do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
       it "画像がないと出品できない" do
         @item.image = nil
         @item.valid?
