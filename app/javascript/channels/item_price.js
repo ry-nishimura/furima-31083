c0bfc21e6b50a1deb0fd6dd46f77cc22ec49e26a
ry-nishimura <72368098+ry-nishimura@users.noreply.github.com>
2020-11-11 17:06:46 +0900

    金額を入力するとコンソール上に金額が表示される

--- app/javascript/channels/item_price.js
+++ app/javascript/channels/item_price.js
@@ -1,3 +1,9 @@
 window.addEventListener('load', () => {
-  console.log("OK");
+  const priceInput = document.getElementById("item-price")
+
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    console.log(inputValue);
+    
+  })
 });
