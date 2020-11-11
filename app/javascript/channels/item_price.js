73ae943029f5b1ce15310e5161cc166491320525
ry-nishimura <72368098+ry-nishimura@users.noreply.github.com>
2020-11-11 18:09:27 +0900

    販売手数料と販売利益が表示される

--- app/javascript/channels/item_price.js
+++ app/javascript/channels/item_price.js
@@ -1,9 +1,13 @@
 window.addEventListener('load', () => {
-  const priceInput = document.getElementById("item-price")
+  const priceInput = document.getElementById("item-price");
 
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-    console.log(inputValue);
     
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
+    
+    const addSalesProfit = document.getElementById("profit");
+    addSalesProfit.innerHTML = inputValue - Math.floor(inputValue * 0.1);
   })
 });
