12e867e4acb7357001b467a11ba04544d4414c76
ry-nishimura <72368098+ry-nishimura@users.noreply.github.com>
2020-11-11 19:02:55 +0900

    単体テストコードを追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -32,25 +32,50 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Explanation can't be blank")
       end
+      it "商品のカテゴリーが空だと保存できない" do
+        @item.category_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
       it "商品のカテゴリーが---だと保存できない" do
         @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Category must be other than 1")
+      end
+      it "商品の状態が空だと保存できない" do
+        @item.condition_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
       end
       it "商品の状態が---だと保存できない" do
         @item.condition_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Condition must be other than 1")
       end
+      it "配送料の負担が空だと保存できない" do
+        @item.shipping_fee_type_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping fee type can't be blank")
+      end
       it "配送料の負担が---だと保存できない" do
         @item.shipping_fee_type_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping fee type must be other than 1")
       end
+      it "発送元の地域が空だと保存できない" do
+        @item.prefecture_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
       it "発送元の地域が---だと保存できない" do
         @item.prefecture_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
+      end
+      it "発送までの日数が空だと保存できない" do
+        @item.delivery_time_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery time can't be blank")
       end
       it "発送までの日数が---だと保存できない" do
         @item.delivery_time_id = 1
