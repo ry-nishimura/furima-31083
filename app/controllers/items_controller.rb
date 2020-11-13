5318c960dd870d5e839d0f4f45ca03731bb9d634
ry-nishimura <72368098+ry-nishimura@users.noreply.github.com>
2020-11-13 18:24:23 +0900

    出品者以外のユーザーが編集ページのURLを直打ちしてもトップページに遷移するよう修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_index, except: [:index, :show, new, :create]
   
   def index
     @items = Item.all.order("created_at DESC")
@@ -40,5 +41,12 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless current_user.id == @item.user_id
+      redirect_to action: :index
+    end
   end
 end
