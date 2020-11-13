126b2aa9f62fba76546eec0c9d5ffd2e0de15532
ry-nishimura <72368098+ry-nishimura@users.noreply.github.com>
2020-11-13 12:32:28 +0900

    itemsコントローラーにeditアクションと、updateアクションを定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,12 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+  end
+
+  def update
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :explanation, :category_id, :condition_id, :shipping_fee_type_id, :prefecture_id, :delivery_time_id, :price, :image).merge(user_id: current_user.id)
