4904808ea82ec91543fbc767172a49bb2460ac6d
ry-nishimura <72368098+ry-nishimura@users.noreply.github.com>
2020-11-16 18:41:46 +0900

    attr_accessorを修正

--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -1,6 +1,10 @@
 class UserOrder
   include ActiveModel::Model
-  attr_accessor :number, :exp_month, :exp_year, :cvc, :postal_code, :prefecture, :city, :address, :building_name, :phone_number
+  attr_accessor :token, :postal_code, :prefecture, :city, :address, :building_name, :phone_number
   
   validates :token, presence: true
+  with_options precence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+
+  end
 end
