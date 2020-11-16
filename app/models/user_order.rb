50f0afc96ab36fdcc26c853ff9d56287965bda60
ry-nishimura <72368098+ry-nishimura@users.noreply.github.com>
2020-11-16 18:32:57 +0900

    attr_accessorを設定

--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -1,3 +1,5 @@
 class UserOrder
   include ActiveModel::Model
+  attr_accessor :number, :exp_month, :exp_year, :cvc, :postal_code, :prefecture, :city, :address, :building_name, :phone_number
+  
 end
