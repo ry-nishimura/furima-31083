0ed44eb81d496d0fcb8a2166934a7bf1b5cedc86
ry-nishimura <72368098+ry-nishimura@users.noreply.github.com>
2020-11-17 18:34:30 +0900

    カード情報を取得し、変数cardに代入

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,8 +1,18 @@
 const pay = () => {
+  Payjp.setPublicKey("pk_test_9b0f6b93ae704bc07ce48109");
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log("フォーム送信時にイベント発火")
+    
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("order[number]"),
+      exp_month: formData.get("order[exp_month]"),
+      exp_year: `20${formData.get("order[exp_year]")}`,
+      cvc: formData.get("order[cvc]"),
+    }
   });
 };
 
