20bcb9a553a1ccad605f4c47a9425e64a87ce68c
ry-nishimura <72368098+ry-nishimura@users.noreply.github.com>
2020-11-17 13:53:42 +0900

    PurchaseInfoモデルのマイグレーションファイルを編集

--- db/migrate/20201117033435_create_purchase_infos.rb
+++ db/migrate/20201117033435_create_purchase_infos.rb
@@ -1,7 +1,13 @@
 class CreatePurchaseInfos < ActiveRecord::Migration[6.0]
   def change
     create_table :purchase_infos do |t|
-
+      t.string     :postal_code,     null:false
+      t.integer    :prefecture_id,   null:false
+      t.string     :city,            null:false
+      t.string     :address,         null:false
+      t.string     :building_name
+      t.string     :phone_number,    null:false
+      t.references :purchase_manage, foreign_key:true
       t.timestamps
     end
   end
