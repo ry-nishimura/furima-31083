dc45582dae428814c5b1a3fca5e3fa312e5c9104
ry-nishimura <72368098+ry-nishimura@users.noreply.github.com>
2020-11-17 12:06:49 +0900

    Revert "購入情報のバリデーションを修正"
    This reverts commit d08fc46b22769b8316a41f623bd7dac8f4a3c3bb.

--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -8,6 +8,6 @@ class UserOrder
     validates :prefecture, format: numericality: { other_than: 1 }
     validates :city
     validates :address
-    validates :phone_number format: { /^(0{1}\d{9,10})$/, message: "is invalid. Not include hyphen(-)" }
+    validates :phone_number format: { /^(0{1}\d{9,10})$/, message: "is invalid. Include hyphen(-)" }
   end
 end
