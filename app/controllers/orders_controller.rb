9ea7c95e711fec7d9f7f523127ea817992a0fb04
ry-nishimura <72368098+ry-nishimura@users.noreply.github.com>
2020-11-17 20:11:23 +0900

    フォームから送信された値が保存されるように、indexアクションとcreateアクションを編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,16 @@
 class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    @order_purchase_info = OrderPurchaseInfo.new
   end
 
   def create
+    @order_purchase_info = OrderPurchaseInfo.new(purchase_info_params)
+    if @order_purchase_info.valid?
+      @order_purchase_info.save
+      redirect_to controller: :items, action: :index
+    else
+      render action: :index
+    end
   end
 end
