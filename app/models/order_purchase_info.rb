0c099324a91629dff1419ecdf17c08a760cc2b14
ry-nishimura <72368098+ry-nishimura@users.noreply.github.com>
2020-11-17 20:32:01 +0900

    order_purchase_info.rbのバリデーションを修正

--- app/models/order_purchase_info.rb
+++ app/models/order_purchase_info.rb
@@ -3,12 +3,12 @@ class OrderPurchaseInfo
   attr_accessor :token, :postal_code, :prefecture, :city, :address, :building_name, :phone_number
   
   validates :token, presence: true
-  with_options precence: true do
+  validates :prefecture, numericality: { other_than: 1, message: "can't be blank" }
+  with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture, format: numericality: { other_than: 1 }
     validates :city
     validates :address
-    validates :phone_number format: { with: /^(0{1}\d{9,10})$/, message: "is invalid. Not include hyphen(-)" }
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "is invalid. Not include hyphen(-)" }
   end
   def save
     PurchaseInfo.create(postal_code: postal_code, prefecture: prefecture, city: city, address: address, building_name: building_name, phone_number: phone_number)
