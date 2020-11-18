348892c05adafbde51de6da00c6a8a3d7a1e0142
ry-nishimura <72368098+ry-nishimura@users.noreply.github.com>
2020-11-18 12:30:41 +0900

    フォームからデータを取得するための記述を修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -8,10 +8,10 @@ const pay = () => {
     const formData = new FormData(formResult);
 
     const card = {
-      number: formData.get("order[number]"),
-      exp_month: formData.get("order[exp_month]"),
-      exp_year: `20${formData.get("order[exp_year]")}`,
-      cvc: formData.get("order[cvc]"),
+      number: formData.get("order_purchase_info[number]"),
+      exp_month: formData.get("order_purchase_info[exp_month]"),
+      exp_year: `20${formData.get("order_purchase_info[exp_year]")}`,
+      cvc: formData.get("order_purchase_info[cvc]"),
     };
 
     Payjp.createToken(card, (status, response) => {
