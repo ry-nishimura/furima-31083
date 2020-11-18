466a54250fec0823589fb90413e715c571a907b9
ry-nishimura <72368098+ry-nishimura@users.noreply.github.com>
2020-11-18 16:47:26 +0900

    カード情報の「期限年」が削除されるように修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -24,7 +24,7 @@ const pay = () => {
 
       document.getElementById("card-number").removeAttribute("name");
       document.getElementById("card-exp-month").removeAttribute("name");
-      document.getElementById("card-exp-month").removeAttribute("name");
+      document.getElementById("card-exp-year").removeAttribute("name");
       document.getElementById("card-cvc").removeAttribute("name");
 
       document.getElementById("charge-form").submit();
