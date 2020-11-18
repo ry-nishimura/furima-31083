1e32e4bb0ea7e7f4b07dd479c052dde71e4d0629
ry-nishimura <72368098+ry-nishimura@users.noreply.github.com>
2020-11-18 19:39:55 +0900

    @itemを定義し、商品の値段の情報を取得

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,10 +5,12 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @order_purchase_info = OrderPurchaseInfo.new(purchase_info_params)
     if @order_purchase_info.valid?
       Payjp.api_key = "sk_test_2ced90ecfbb0cffe9f35ec0a"
       Payjp::Charge.create(
+        amount: @item.price,
         card: purchase_info_params[:token],
         currency: 'jpy'
       )
