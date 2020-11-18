6dd4cc3c9ee3fea9c65c5b9536c50590a923c305
ry-nishimura <72368098+ry-nishimura@users.noreply.github.com>
2020-11-18 22:30:17 +0900

    prefectureをprefecture_idに修正

--- app/models/order_purchase_info.rb
+++ app/models/order_purchase_info.rb
@@ -1,9 +1,9 @@
 class OrderPurchaseInfo
   include ActiveModel::Model
-  attr_accessor :token, :postal_code, :prefecture, :city, :address, :building_name, :phone_number
+  attr_accessor :token, :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number
   
   validates :token, presence: true
-  validates :prefecture, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city
@@ -11,7 +11,7 @@ class OrderPurchaseInfo
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "is invalid. Not include hyphen(-)" }
   end
   def save
-    PurchaseInfo.create(postal_code: postal_code, prefecture: prefecture, city: city, address: address, building_name: building_name, phone_number: phone_number)
+    PurchaseInfo.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phone_number: phone_number)
     Order.create(token: token)
   end
 end
