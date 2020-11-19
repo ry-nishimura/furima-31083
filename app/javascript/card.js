dd38eb53493bda69fa385dae3d9fdb5f8f2478e4
ry-nishimura <72368098+ry-nishimura@users.noreply.github.com>
2020-11-19 12:09:54 +0900

    環境変数を呼び出せるように設定

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_9b0f6b93ae704bc07ce48109");
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
