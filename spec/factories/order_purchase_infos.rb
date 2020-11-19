86bb429a54cf5c1a684f89904ce2267f56a0edc2
ry-nishimura <72368098+ry-nishimura@users.noreply.github.com>
2020-11-19 14:38:38 +0900

    FactoryBotの修正

--- spec/factories/order_purchase_infos.rb
+++ spec/factories/order_purchase_infos.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :order_purchase_info do
     token {"tok_abcdefghijk00000000000000000"}
     postal_code { '123-4567' }
-    prefecture { '15' }
+    prefecture_id { '15' }
     city { '横浜市緑区' }
     address { '青山1-1-1' }
     building_name { '柳ビル123' }
